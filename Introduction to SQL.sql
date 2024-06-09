2a539750f82949f06d133a23d7e6993a44788534
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-06-09 15:13:48 +0530

    Add files via upload
    This file contain introduction about SQL
    - Introduced in 1979 by IBM
    - SQL is a software which used to interact with Databases and its Relational Database Management

---
+++ Introduction to SQL.sql
@@ -0,0 +1,18 @@
+-- We can say it as SEQUEL OR SQL
+-- SEQUEL : Structured English Query language.
+-- SQL : Structured Query Language. Earlier it was named as SEQUEL but now we are saying as SQL and it was introduced by IBM 1979
+-- Database is a collection of data and easily accessed and the data will be in the form of digital
+-- A software application used to manage our data is called Database Management System
+-- SQL is used to acces the the DBMS
+-- There are two types of Data 1. Relational 2. Non-relational
+-- 1. Relational Database means where the data is stored in the form of tables (Rows & coloumns) | Mysql , Oracle , postgrell ,etc
+-- 2. NonRelational Database is called no sql is doesnt store the data in the form of table | eg MongoDB
+
+-- SQL is a programming language which is used to interact with Relational Database
+-- SQL is used to perform the CRUD operation | CREATE , READ , UPDATE , DELETE
+
+-- Database can contian multiple tables and data is stored inside the tables. it store the data in inter related format
+
+-- CREATE DATABASE DATABASE_NAME   is used to create the database inside database we can store the table 
+-- USE DATABASE_NAME;
+-- create the table name CREATE TABLE TABLE_NAME
