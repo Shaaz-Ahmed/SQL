16525be89001053a403eb5f7317c39e3dd8831c6
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-09-11 18:41:52 +0530

    WHERE clause
    It is used yo filter records in a query, return only rows that meet a specified conditions

---
+++ Clauses/WHERE clause.sql
@@ -0,0 +1,10 @@
+-- WHERE Clause: It is used yo filter records in a query, return only rows that meet a specified conditions
+
+use emp_detail;
+
+SELECT * FROM Employee;
+
+-- WHERE Condition
+SELECT Emp_Salary from Employee WHERE EMp_Salary > 30000;
+SELECT Emp_City FROM Employee WHERE Emp_City = "Mumbai";
+SELECT 
