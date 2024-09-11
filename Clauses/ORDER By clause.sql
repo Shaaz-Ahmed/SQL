33f6eaaf3bc61065941bf39368a407067f512324
Shaaz Mukadam <shaazmukadam1616@gmail.com>
2024-09-11 23:24:07 +0530

    Order clause
    It gives the values in ascending or descending format.

---
+++ Clauses/ORDER By clause.sql
@@ -0,0 +1,13 @@
+-- order by : order by clause is used to put the data either in ascending or descending format
+
+use emp_detail;
+
+SELECT * FROM Employee;
+
+SELECT * FROM Employee ORDER BY Emp_Name ASC;
+
+SELECT * FROM Employee ORDER BY Emp_Salary ASC;
+
+SELECT * FROM Employee ORDER BY Emp_Salary DESC;
+
+SELECT * FROM Employee ORDER BY Emp_Name;
